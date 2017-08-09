e45758d6f5f65b05e53b510cd6c773d96bc08f2c
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-09 13:59:29 -0500

    Print data to console.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,12 +1,14 @@
 puts "What is the hamster's name?"
 name = gets.chomp
+puts "Hamster's name is #{name}."
 
 puts "What is the hamster's volume level from 1-10?"
 volume_level = gets.chomp.to_i
+puts "Hamster's name is #{volume_level}."
 
 puts "What is the hamster's fur color?"
 fur_color = gets.chomp
-
+puts "Hamster's fur color is #{fur_color}"
 
 valid_input = false
 
@@ -19,9 +21,11 @@ input = gets.chomp
 if input == "y"
   good_candidate = true
   valid_input = true
+  puts "Hamster is a good candidate for adoption."
 elsif input == "n"
   good_candidate = false
   valid_input = true
+  puts "Hamster is not a good candidate for adoption."
 else
   puts "Please enter y/n as a response."
 end #ending conditional statement
@@ -30,5 +34,6 @@ end #ending until loop
 
 puts "What is the hamster's estimated age?"
 estimated_age = nil
+puts "Hamster's estimated age is #{estimate_age}."
 
 
