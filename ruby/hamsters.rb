c696a5dcf97d63195e7eb38c2b1d8aaed6d482e5
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-09 13:54:31 -0500

    Add valid input conditional loop to good candidate response.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,14 +1,34 @@
 puts "What is the hamster's name?"
 name = gets.chomp
 
-puts "What is your volume level from 1-10?"
-volume_level = gets.chomp
+puts "What is the hamster's volume level from 1-10?"
+volume_level = gets.chomp.to_i
 
-puts "What is your fur color?"
+puts "What is the hamster's fur color?"
 fur_color = gets.chomp
 
-puts "Is hamster a good candidate for adoption? (y/n)?"
-good_candidate = gets.chomp
+
+valid_input = false
+
+until valid_input
+#until has to be ended
+
+puts "Is hamster a good candidate for adoption? (y/n)"
+input = gets.chomp
+
+if input == "y"
+  good_candidate = true
+  valid_input = true
+elsif input == "n"
+  good_candidate = false
+  valid_input = true
+else
+  puts "Please enter y/n as a response."
+end #ending conditional statement
+
+end #ending until loop
 
 puts "What is the hamster's estimated age?"
 estimated_age = nil
+
+
