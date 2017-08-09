3eaabacc2f0a81b926278fbe4af4e281fcde9f00
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-09 13:29:48 -0500

    Add list of hamster related questions.

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+
+puts "What is your volume level from 1-10?"
+volume_level = gets.chomp
+
+puts "What is your fur color?"
+fur_color = gets.chomp
+
+puts "Is hamster a good candidate for adoption? (y/n)?"
+good_candidate = gets.chomp
+
+puts "What is the hamster's estimated age?"
+estimated_age = nil
