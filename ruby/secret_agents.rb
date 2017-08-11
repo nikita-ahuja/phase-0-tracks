dbb8e7d9874c48c0801f31fb4b323006ebc57288
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-11 13:49:42 -0500

    play with ways to enforce decrypt method. Add alphabet string in comments.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -14,7 +14,7 @@ string = gets.chomp
 # loop through each individual character
 while index < string.length
   # using the "next" function
-  string[index] = string[index].next
+  string[index] = string[index].next!
   index += 1
 end
 
@@ -22,11 +22,29 @@ puts string
 
 end
 
-encrypt()
-
 
 # Pseudocode the following method:
 
 # A decrypt method that reverses the process above. Note that Ruby doesn't have a built-in method for going backward one letter. How can you find out where a letter is in the alphabet, then access the letter right before it? Hint: In the IRB session above, you learned how to ask a string for the index of a letter. "abcdefghijklmnopqrstuvwxyz" counts as a string.
 
-#define a decrypt method
+#define a decrypt method and call encrypt
+def decrypt ()
+#we have our reversed string (theoretically)
+#"bcd"
+#cycle through each character
+while index < string.length
+  # using the "next" function
+  string[index] = string[index] #mess with the right hand index.
+  #alphabet_string = "abcdefghijklmnopqrstuvwxyz"
+  #if you have the letter b as string[index], it will always need to equal
+  #alphabet_string.index[0]
+  index += 1 #cycles through each character chronologically
+end
+
+puts string
+
+end
+
+
+# and reverse it one
+#call the encrypt method within the decrypt method
