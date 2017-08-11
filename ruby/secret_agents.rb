dbc21fc6cf47a7e640fab0c9dcc4657fe88734c3
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-11 02:57:59 -0500

    Added comments to create outline for partner challenge.

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,12 @@
+# Pseudocode the following method:
+
+# An encrypt method that advances every letter of a string one letter forward. So "abc" would become "bcd". For now, you can assume lowercase input and output. Any space character should remain a space character -- no change made.
+
+#Actual coded method:
+
+
+# Pseudocode the following method:
+
+# A decrypt method that reverses the process above. Note that Ruby doesn't have a built-in method for going backward one letter. How can you find out where a letter is in the alphabet, then access the letter right before it? Hint: In the IRB session above, you learned how to ask a string for the index of a letter. "abcdefghijklmnopqrstuvwxyz" counts as a string.
+
+#Actual coded method:
