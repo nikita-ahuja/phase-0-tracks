874c76f76fadc592591cacc50155a6f892c7103e
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-12 20:11:30 -0500

    Complete decrypt method successfully

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -27,24 +27,23 @@ end
 
 # A decrypt method that reverses the process above. Note that Ruby doesn't have a built-in method for going backward one letter. How can you find out where a letter is in the alphabet, then access the letter right before it? Hint: In the IRB session above, you learned how to ask a string for the index of a letter. "abcdefghijklmnopqrstuvwxyz" counts as a string.
 
-#define a decrypt method and call encrypt
-def decrypt ()
-#take the encrypted word..."bcd"
-#cycle through each character
+# and reverse it one
+#call the encrypt method within the decrypt method
+
+
+def decrypt(string)
+#take a string as an argument
+#make each letter to transform into the letter before it in the alphabet
+
+alpha = "abcdefghijklmnopqrstuvqxyz"
+index = 0
+#new_character = alpha[((alpha.index(string[index])-1))]
+
 while index < string.length
-#when it gets to the first character, revert it back to the character that is alphabetically before it
-  # using the "next" function
-  string[index] = alphabet_string[index] - 1 #mess with the right hand index.
-  #alphabet_string = "abcdefghijklmnopqrstuvwxyz"
-  #if you have the letter b as string[index], it will always need to equal
-  #alphabet_string.index[0]
-  index += 1 #cycles through each character chronologically
+  string[index] = alpha[((alpha.index(string[index])-1))]
+  index += 1
 end
 
 puts string
 
 end
-
-
-# and reverse it one
-#call the encrypt method within the decrypt method
