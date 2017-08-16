22727e4724a582eab05bbac41cadf9e39dbb0611
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-16 00:29:17 -0500

    Add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PSEUDOCODE:
+# 1. Create a form with labels to add designated information
+# 2. Ask the user to fill out the form and allow the user to do so
+# 3. Print the form back out to the user for review
+# 4. Ask the user if there are any values they would like to update
+# - provide a list of key's for the user to reference
+# 5. Unless the user says "none", skip this.
+# 6. If the user enters a key name, ask for a new value.
+# 7. After the user enters the value, update the key with the new value.
+# 8. Print the final version of the form
+# 9. Exit the program.
