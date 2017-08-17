9d8a5f11aee800db991dc0bc424ffc4a7d93cd10
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-16 22:18:00 -0500

    Add nested data up to personal care

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,52 @@
 # GROCERY STORE
 
-grocery_store = {}
+grocery_store = {
+#PRODUCE - VEG & FRUIT
+  produce_section: {
+    vegetables: ["cucumbers", "tomatoes", "celery"],
+    fruit: ["apples", "oranges", "dragonfruit"]
+  },
+#DAIRY
+  dairy_section: ["milk", "yogurt", "eggs"],
+#CEREAL - DRY & BOXED
+  cereal: {
+    oats: ["Quaker Oats", "Steel Cut", "Instant Oats"],
+    boxed: ["Honey Bunches", "Cheerios", "Kellogs"]
+  },
+#SNACKS
+snacks: ["almonds", "chips", "rice cakes"],
+#COSMETICS
+personal_care: {
+  shampoo_condit: ["Garnier Fructisse", "Pantene Pro-V", "Dove"],
+  makeup: ["lipstick", "nail polish", "eyeshadow"],
+  face_wash: ["Cerave", "Clearasil Ultra", "Cetaphil"],
+  }
+}
+
+p grocery_store
+
+# holiday_supplies = {
+#   "winter" => {
+#     "Christmas" => ["lights", "tree"],
+#     "New Years" => "champagne glasses"
+#   },
+#   "summer" => {
+#     "July Fourth" => ["BBQ", "flags"]
+#   },
+#   "spring" => {
+#     "Memorial Day" => "BBQ"
+#   },
+#   "fall" => {
+#     "Labor Day" => "hot dogs"
+#   }
+# }
+
+# p holiday_supplies
+
+
+produce_section = {
+  fruit: ["apples", "oranges", "dragonfruit"],
+  vegetables: ["cucumbers", "tomatoes", "celery"]
+}
+
+p produce_section
