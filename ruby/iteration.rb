da3a62ed2e77071c6ebe5144d65911b924a43428
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-17 16:10:55 -0500

    Modify cities array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,34 @@ def say_hello
 end
 
 say_hello {|name1, name2, name3| puts "Great to see you #{name1}, #{name2}, #{name3}!"}
+
+
+cities_in_texas = ["Austin", "Houston", "Dallas"]
+
+#before print
+puts "This is the array before modification:"
+p cities_in_texas
+
+#modify array
+cities_in_texas.map! do |city|
+  puts city.downcase + "!"
+end
+
+#after print
+puts "This is the array after modification:"
+p cities_in_texas
+
+
+
+state_capitals = {
+  Georgia: "Atlanta",
+  Texas: "Austin",
+  Virginia: "Richmond",
+}
+
+#before print
+puts "This is the hash before modification:"
+p state_capitals
+
+
+#modify hash
