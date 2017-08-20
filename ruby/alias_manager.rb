aef3f2e8042f1ed7d236e83d4ade64ac3eb77660
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-19 19:59:03 -0500

    attempting vowel change function

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,57 @@
-def name_swap(real_name)
+def swap_name(real_name)
   name_array = real_name.split(" ")
   name_array[0], name_array[1] = name_array[1], name_array[0]
-  p name_array
+  #p name_array
 end
 
 
+def vowel_change(real_name)
+  vowel = "aeiou"
+  real_name = real_name.chars
+  real_name.map! do |letter|
+    #letter.each
+    vowel.index(real_name[letter]) != nil
+      #letter = vowel[index+1]
+      p letter
+    end
+end
+  #p real_name
+   #now run something
+   #join back the characters
+    #something in regards to nil... refer to robot translator code
+#end
 
-name_swap("Felicia Torres")
+#vowel_change { |letter| letter.map! puts real_name }
+
+#vowel_change("Nikita")
+
+
+# def consonant_change(real_name)
+#   vowels = "aeiou"
+#   i=0
+#   real_name.chars
+# end
+
+
+  #real_name = "nikita"
+#   def vowel_change(real_name)
+#     vowel = "aeiou"
+#     vowel=vowel.chars
+#     real_name = real_name.chars
+#     index = 0
+#     while index < real_name.join.length
+#       if vowel.index(real_name[index]) != nil
+#         real_name[index] = vowel[index+1]
+#       else
+#         index += 1
+#       end
+#     end
+#     p real_name.join
+#   end
+
+# vowel_change("nikita")
+
+
 
 
 
@@ -17,4 +62,8 @@ name_swap("Felicia Torres")
 #2. swap first and last name
 # - split the name into 2 by using the split method
 # - swap the array values
-#3. define another method
+#3. define another method that changes the vowels
+#- split the name into characters in an array
+#- using the each loop circulate through each letter and change it to the next vowel
+#- join back the characters
+#4. define another method that changes the consonants
