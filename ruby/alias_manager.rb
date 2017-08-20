337749a5a4bcacb4e58ab0aa323e7f0cf47a8124
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-19 23:16:40 -0500

    Use rotate function for arrays

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,19 +6,47 @@ end
 
 
 def vowel_change(real_name)
+  vowel = ["a", "e", "i", "o", "u"]
+  real_name = real_name.chars
+  real_name.map! do |letter|
+    if letter == vowel
+      letter.rotate(1)
+    else
+      letter
+    end
+  end
+  p real_name
+end
+
+vowel_change("Nikita")
 
 
+#   #index=0
+#   #while index < vowels.length
+#   #real_name.include? ("a") {|letter| p "real name has a" }
+#   real_name.include? (vowels)
+#   yield
+#     #else
+#       #p "real name does not have" + " " + (vowels[index])
+#       #p real_name
+#     #index += 1
+#     #end
+# end
 
 
+# SWAP NAME PSEUDOCODE
+# 1. Define a method that take the full real name
+# 2. using the split function for strings, split the full name into an array with two indexes
+#-real_name.split[0] == first name and [1] == last name
+#- switch the two index values
+#-done!
 
 
-#PSEUDOCODE
-#1. define method that take a spy's full name
-#2. swap first and last name
-# - split the name into 2 by using the split method
-# - swap the array values
-#3. define another method that changes the vowels
-#- split the name into characters in an array
-#- using the each loop circulate through each letter and change it to the next vowel
-#- join back the characters
-#4. define another method that changes the consonants
+# VOWEL CHANGE PSEUDOCODE
+#1. define a method that takes the full real name
+#2. define a variable for vowels
+#3. turn the real name into an array
+#real_name.include?(vowel)
+
+
+# CONSONANT CHANGE PSEUDOCODE
