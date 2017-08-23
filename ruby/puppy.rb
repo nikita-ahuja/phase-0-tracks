e55104511b61f2656d4cd308fa52c9219e328147
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-22 23:13:27 -0500

    Add characteristics and behaviors of puppy class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,2 +1,23 @@
-class Puppy
-end
+# How might you design a Puppy class? What sorts of characteristics and behavior might
+# a Puppy class involve? Using a format similar to the one we used for Eyelashus
+# Adorableus, come up with some characteristics and behavior for a Puppy class. Use
+# comments to add your class design to phase-0-tracks/ruby/puppy.rb.
+
+# CLASS ------------------------
+# Puppy !!!
+
+# ATTRIBUTES (CHARACTERISTICS) ----------------
+# Amount of legs: 4
+# Friendly: varies
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Tail?: Yes
+# Ears: 2
+
+
+# MODULES (BEHAVIOR) -----------------------
+# Jump
+# Lick
+# Hug
+# Wag Tail
