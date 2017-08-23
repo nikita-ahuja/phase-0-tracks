9c9c2d6843b371000f34685b63a90e277e89d304
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-23 16:14:21 -0500

    Complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -11,6 +11,21 @@ class Puppy
     end
   end
 
+  def roll_over()
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_years = human_years/7
+    return dog_years
+  end
+
+  def do_flips(amount_of_flips)
+    amount_of_flips.times do |i|
+      puts "The dog flips #{i} times!"
+    end
+  end
+
 end
 
 
@@ -20,4 +35,7 @@ end
 puppy = Puppy.new()
 puppy.fetch("Gorilla toy")
 puppy.speak(3)
+puppy.roll_over
+p puppy.dog_years(21)
+puppy.do_flips(3)
 
