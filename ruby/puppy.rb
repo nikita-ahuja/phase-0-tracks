36456d73d50c3f7e96b7775415f6fa73251e1721
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-23 15:42:34 -0500

    Add names

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,3 +1,5 @@
+#MANDATAORY PAIRING: Nikita Ahuja & Joseph Lesca
+
 # How might you design a Puppy class? What sorts of characteristics and behavior might
 # a Puppy class involve? Using a format similar to the one we used for Eyelashus
 # Adorableus, come up with some characteristics and behavior for a Puppy class. Use
