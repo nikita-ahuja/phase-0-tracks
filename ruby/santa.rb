0b3e4726485659f569e1e4f51d2baa55c3757ca0
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-23 15:25:57 -0500

    Add attributes and instance variables

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
 
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -8,15 +16,8 @@ class Santa
     puts "That was a good type of #{cookie_type}!"
   end
 
-  def initialize()
-    puts "Initializing Santa instance..."
-  end
-
 end
 
 # DRIVER CODE:
 # Santa.new.speak
-
 # Santa.new.eat_milk_and_cookies("Oreo")
-
-# #Santa.new.initialize
