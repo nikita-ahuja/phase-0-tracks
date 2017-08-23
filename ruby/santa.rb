335ca87b81cde1e20046da828ef623a91f4d0177
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-23 01:40:40 -0500

    Add 3 instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,2 +1,22 @@
 class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good type of #{cookie_type}!"
+  end
+
+  def initialize()
+    puts "Initializing Santa instance..."
+  end
+
 end
+
+# DRIVER CODE:
+# Santa.new.speak
+
+# Santa.new.eat_milk_and_cookies("Oreo")
+
+# #Santa.new.initialize
