330cf2f8f130bb6bcf7dbbd60e2fbcfed43bae2b
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-24 04:29:46 -0500

    replace module methods with classes and instantiate each class and call the module method on each

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,36 @@
 module Shout
-
-  def self.yell_angrily(words)
-    puts words + "!!!" + " :("
+  def yell_loudly(words)
+    puts words.upcase + "!!!!!!"
   end
-
-  def self.yelling_happily(words)
-    puts words + " " + "WOOOOO!!!! :D "
-  end
-
 end
 
-Shout.yell_angrily("UGH")
-Shout.yelling_happily("Hi")
+
+class Announcer
+  include Shout
+end
+
+
+class Bouncer
+  include Shout
+end
+
+  # def self.yell_angrily(words)
+  #   puts words + "!!!" + " :("
+  # end
+
+  # def self.yelling_happily(words)
+  #   puts words + " " + "WOOOOO!!!! :D "
+  # end
+
+#end
+
+# DRIVER CODE
+
+# Shout.yell_angrily("UGH")
+# Shout.yelling_happily("Hi")
+
+club_bouncer = Bouncer.new #instantiate
+club_bouncer.yell_loudly("You can't come in here")
+
+sports_announcer = Announcer.new #instantiate
+sports_announcer.yell_loudly("...and he misses the shot")
