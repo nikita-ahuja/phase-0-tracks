a7d964e56a17cccb9097f929bab2ee231529319c
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-23 23:03:47 -0500

    Add more initializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -21,3 +21,36 @@ end
 # DRIVER CODE:
 #santa = Santa.new("Male", "Latino")
 #Santa.new("Male", "Latino").eat_milk_and_cookies("Oreo")
+
+
+santas = []
+
+# EXAMPLE INITIALIZATIONS
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+# MORE INITIALIZATIONS
+
+santas << Santa.new("female", "Latina")
+
+10.times do
+  puts "Creating a new agender, black Santa."
+  santas << Santa.new("agender","black")
+end
+
+# p santas.length
+
+
+
+
