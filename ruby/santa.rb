2693fb771a09b499ee7d112183b76f8f530127e8
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-08-24 02:50:28 -0500

    Create 100 more santas with random ages, genders, and ethnicities woohooooo

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,7 +7,7 @@ class Santa
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = rand(0..140)
   end
 
   def speak
@@ -30,8 +30,8 @@ class Santa
 end
 
 # DRIVER CODE:
-santa = Santa.new("Male", "Latino")
-Santa.new("Male", "Latino").eat_milk_and_cookies("Oreo")
+santa1 = Santa.new("Male", "Latino")
+santa2 = Santa.new("Male", "Latino").eat_milk_and_cookies("Oreo")
 
 
 santas = []
@@ -43,13 +43,13 @@ example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
 # MORE INITIALIZATIONS
 
@@ -58,6 +58,13 @@ santas << Santa.new("female", "Latina")
 10.times do
   puts "Creating a new agender, black Santa."
   santas << Santa.new("agender","black")
+end
+
+100.times do |i|
+  new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  puts "New santa #{i} gender is #{new_santa.gender}."
+  puts "New santa #{i} ethnicity is #{new_santa.ethnicity}."
+  puts "New santa #{i} age is #{new_santa.age}."
 end
 
 # p santas.length
