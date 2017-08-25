96ad0bcbd37c64e2b3c96d3e34eb0a947ab609ff
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-25 00:53:35 -0500

    Initial commit

---
+++ ruby/guessing_game/guessing_game.rb
@@ -0,0 +1,44 @@
+#PSEUDOCODE
+
+class Game
+end
+
+
+# #def initialize
+# #Input: none
+# #Steps:
+# - note that the game has begun with a BOOLEAN (@game_over = false)
+# - and that the word is unsolved with another boolean (@word_solved = false)
+# #Output:
+# -the word will be assigned to a “word” variable
+# - ask the other user to guess the word or a letter
+
+# #def guessing(users_guess)
+# #input: users guess (string)
+# #steps:
+# -create a LOOP and IF statement that compares the users guess with the inputted word
+# -create a counter that counts the amount of guesses
+# -limit the amount of guesses to the letters in the word/2 (while guess_counter < word.length/2)
+# -if the letter the user guesses is in the word then print the word for them with that letter included
+# -if a user guesses the same letter twice, do not increment the counter!!!
+# #output: boolean ??
+
+#  if users_guess == word
+#   @game_over = true
+
+# # def show_letter(letter)
+# #input:
+# #steps:
+
+# #end_game
+# if game_over && word_solved
+# puts “Congratulations!”
+# else
+# puts “haha you lost!”
+# end
+
+# Driver code:
+# # Ask the user to enter a word
+# -Assign that word to a variable
+# # Tell user 2 to begin guessing
+
