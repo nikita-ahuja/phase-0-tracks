0b02625730fd1e538fefd76ceb383593a5d7ff24
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-26 11:13:05 -0500

    Add release 2 in comments

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -15,3 +15,19 @@ class Calculator
     x / y
   end
 end
+
+
+# Are there any handy RSpec cheat sheets out there? What observations can you make from skimming them?
+
+# https://www.anchor.com.au/wp-content/uploads/rspec_cheatsheet_attributed.pdf
+
+
+# Where is the RSpec documentation?
+
+
+# What other RSpec matchers are available aside from eq?
+
+#eq, eql, equal
+
+# Can you find any free RSpec tutorials or in-depth resources that might be helpful to bookmark?
+
