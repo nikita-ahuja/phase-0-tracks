a63368f68ea8f04f084148abbbeea32769350023
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-29 02:08:13 -0500

    Update pseduocode and code to match it

--- js/explore.js
+++ js/explore.js
@@ -3,24 +3,23 @@
 // function reverse(string)
 // 1. Input: string
 // 2. Steps:
-// a. replace the first letter with the last letter
-// - (think indexes and a variable for the first index going forward and last index going backward)
-// b. do this for the next two letters in the word
-// - keep repeating until all letters have been switched
+// a. start with a blank string to append letters to (since strings themselves are IMMUTABLE in JavaScript)
+// -start at the final index of the initial string (initialize counter)
+// - print the letter at the final index
+// - add it to the new string (+=)
+// -keep repeating this method FOR EACH LETTER (i--) until you have reached index 0 of the original string
 // c. when the word is done switching characters, print the word
 // 3. Output: a string that has been reversed
 
 
 function reverse(string) {
-
-  for(var i = 0; i < ((string.length-1)/2); i++) {
-    for (var x = (string.length - 1); x > ((string.length-1)/2); x--) {
-      var new_string = ""; string[i] + string[x]
+  var new_string = ""
+  for(var i = string.length-1; i >= 0; i--) {
+      new_string += string[i];
       //console.log(string[i])
       //console.log(string[x])
-    }
   }
-console.log(new_string)
+console.log(new_string);
 }
 
 reverse("hello");
