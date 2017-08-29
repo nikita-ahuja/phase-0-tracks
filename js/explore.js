733f20da6b6c2d260854d4fcce6e2f677f6e4ac2
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-29 02:17:06 -0500

    psuedocode typo correction

--- js/explore.js
+++ js/explore.js
@@ -2,10 +2,10 @@
 // Function: write a function that reverses a string
 // function reverse(string)
 // 1. Input: string
-// 2. Steps:
+// 2. Steps: see below
 // a. start with a blank string to append letters to (since strings themselves are IMMUTABLE in JavaScript)
-// -start at the final index of the initial string (initialize counter)
-// - print the letter at the final index
+// -start at the final index of the initial string (think of where to initialize counter)
+// - navigate to the letter at the final index
 // - add it to the new string (+=)
 // -keep repeating this method FOR EACH LETTER (i--) until you have reached the very first letter (index 0) of the original string
 // c. when the word is done adding all the letters to the new string, print the new string
