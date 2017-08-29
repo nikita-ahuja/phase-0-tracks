6bdc50540866d82d029003cae19ff922f0295ec4
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-28 22:26:41 -0500

    Not understanding how to get tests to pass... but they are drafted out

--- ruby/guessing_game/guessing_game_spec.rb
+++ ruby/guessing_game/guessing_game_spec.rb
@@ -1,6 +1,5 @@
 require_relative "guessing_game"
 
-
 describe Game do
   let (:game) {Calculator.new}
 
@@ -9,6 +8,10 @@ describe Game do
 
   it "shows the guessed letters if they are in the word"
     expect(game.show_letter(guess)). to eq @letters_revealed
+  end
+
+  it "checks the guess after the user has entered it"
+    expect(game.check_guess(guess)). to eq true || false
   end
 
 end
