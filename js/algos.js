8a6f5224b8944eab013884406cec90486c553584
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-30 01:35:09 -0500

    Add pseudocode for release 1 and object1 code

--- js/algos.js
+++ js/algos.js
@@ -23,10 +23,9 @@ function return_longest(array) {
 console.log(longest_item);
 }
 
-//console.log(array_lengths);
-return_longest(["hello", "hello1", "hello11"]);
-return_longest(["1234", "123456", "123"]);
-return_longest(["NA", "Nikita Ahuja", "Ahuja"])
+// return_longest(["hello", "hello1", "hello11"]);
+// return_longest(["1234", "123456", "123"]);
+// return_longest(["NA", "Nikita Ahuja", "Ahuja"])
 
 
 //RELEASE 1: Write a function that takes two objects and checks to see if the objects share at least one key-value pair.
@@ -38,3 +37,23 @@ return_longest(["NA", "Nikita Ahuja", "Ahuja"])
 // yourFunction({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}); #  true
 
 // If no pairs match (and keep in mind that the two objects may not even have any of the same keys), the function should return false. To make your life easier, don't worry about whether a property is a string ('age') or an identifier name (age). Those can be considered equivalent.
+
+// RELEASE 1 PSEUDOCODE
+// Create a function that takes 2 objects as an argument and returns true if the objects have atleast one same key/value pair or false if no pairs match
+//input: 2 objects (object1, object2)
+//steps:
+// 1. iterate through the items in the first object
+// 2. iterate through the items in the second object
+// 3. compare all object to see if there are any key/value pairs that are the same ...?
+//output: true or false
+
+
+function compare_objects(object1) {
+  for (var key in object1) {
+    if (object1.hasOwnProperty(key)) {
+      console.log(key, object1[key]);
+    }
+  }
+}
+
+compare_objects({animal: "Dog", legs: 4})
