4900e770402c48548b90f161daaf7696e29320ab
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-31 01:36:30 -0500

    Complete release 3

--- ruby/assessment_practice/newsroom.rb
+++ ruby/assessment_practice/newsroom.rb
@@ -4,12 +4,32 @@ attr_reader :name
 attr_accessor
 
   def initialize(name, budget)
-    name = @name
-    budget = @budget
+    @name = name
+    @budget = budget
+    @reporters = {}
   end
+
+  def add_reporter(reporter_name)
+    if @reporters.keys.include?(reporter_name)
+      puts "We can't hire them!"
+    else
+      @reporters[reporter_name] = []
+    end
+    @reporters
+  end
+
+  def salary_for(reporter_name)
+    reporter_name.length * 10000
+  end
+
 
 end
 
-room1 = Newsroom.new("Biiiiitch", 100_000)
+p room1 = Newsroom.new("Biiiiitch", 100_000)
+p room1.name
+#p room1.budget
+p room1.add_reporter("Nikita Ahuja")
+p room1.add_reporter("Nikita Ahuja")
+p room1.salary_for("Anderson Cooper")
 
 
