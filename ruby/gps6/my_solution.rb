328935ead1ef3f86e27803f189a4a8c2420876f1
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-08-31 11:39:20 -0500

    Refactor speed method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -47,6 +47,8 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
   end
 
+  #a hash adds to the complexity
+
 
       #ORIGINAL POPULATION DENISTY METHOD NOT DRY
       # predicted deaths is solely based on population density
@@ -66,18 +68,19 @@ class VirusPredictor
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+
+  speed =
 
     if @population_density >= 200
-      speed += 0.5
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
