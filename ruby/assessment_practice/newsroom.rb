bcd72ce10006d1326a8027cfbfee8c841f5682c7
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-09-01 00:38:14 -0500

    Minor refactors according to video. Comments included

--- ruby/assessment_practice/newsroom.rb
+++ ruby/assessment_practice/newsroom.rb
@@ -1,7 +1,7 @@
 class Newsroom
 
 attr_reader :name
-attr_accessor
+attr_accessor :budget
 
   def initialize(name, budget)
     @name = name
@@ -10,12 +10,14 @@ attr_accessor
   end
 
   def add_reporter(reporter_name, skills)
-    if @reporters.keys.include?(reporter_name)
-      puts "We can't hire them!"
-    elsif has_budget?(reporter_name) == false
-      puts "We can't afford them!"
-    else
-      @reporters[reporter_name] = skills
+    if has_budget?(reporter_name)
+      if @reporters.keys.include?(reporter_name)
+        puts "We can't hire another #{reporter_name}!"
+      else
+        @reporters[reporter_name] = skills
+      end
+    else #!has_budget?(reporter_name)
+      puts "We can't afford #{reporter_name}!"
     end
     @reporters
   end
@@ -24,40 +26,60 @@ attr_accessor
     reporter_name.length * 10000
   end
 
+  # def total_salaries
+  #   total_salaries = 0
+  #   @reporters.keys.each do |name|
+  #     total_salaries += salary_for(name)
+  #   end
+  #   total_salaries
+  # end
+
+# more efficient way to write total_salaries
+
   def total_salaries
-    total_salaries = 0
-    @reporters.each do |name, skills|
-      total_salaries += salary_for(name)
+    @reporters.keys.reduce(0) do |sum, name|
+      sum + salary_for(name)
     end
-    total_salaries
   end
 
+  #commented out inefficiency... comparison operator automatically returns true or false!
   def has_budget?(reporter_name)
-    if total_salaries + salary_for(reporter_name) > @budget
-      false
-    else
-      true
-    end
+    #if
+      total_salaries + salary_for(reporter_name) <=  budget
+    #   true
+    # else
+    #   false
+    # end
   end
 
   def friendly_print
     puts "Welcome to the #{@name} Newsroom!"
+    puts #new line
     puts "Your reporting team is:"
     @reporters.each do |reporter, skills|
       #skills.join(", ")
-      puts "-#{reporter}, specializing in #{skills}."
+      puts "-#{reporter}, specializing in #{skills.join(", ")}."
     end
-    p "Thank you for watching! Good night!"
+    puts
+    puts "Thank you for watching! Good night!"
   end
 
+  # def find_reporters_with_skill(skill)
+  #   reporters_with_skill = []
+  #   @reporters.each do |reporter, skills|
+  #     if skills.include?(skill)
+  #       reporters_with_skill.push(reporter)
+  #     end
+  #   end
+  # p reporters_with_skill
+  # end
+
+  # more efficient way to write skill method
   def find_reporters_with_skill(skill)
-    reporters_with_skill = []
-    @reporters.each do |reporter, skills|
-      if skills.include?(skill)
-        reporters_with_skill.push(reporter)
-      end
+    reporters_with_skill = @reporters.select do |name, skills|
+      skills.include?(skill)
     end
-  p reporters_with_skill
+    reporters_with_skill.keys
   end
 
 end
@@ -66,13 +88,14 @@ p room1 = Newsroom.new("Elysium", 300_000)
 p room1.name
 p room1.add_reporter("Anderson Cooper", ["politics", "economics", "grey hair"])
 p room1.salary_for("Anderson Cooper")
-# p room1.add_reporter("Wolf Blitzer")
+p room1.add_reporter("Wolf Blitzer", ["grey hair"])
 # p room1.add_reporter("Jim Acosta")
 p room1.total_salaries
 p room1.has_budget?("Rachel Maddow")
 p room1.add_reporter("Rachel Maddow", ["speaking", "presenting"])
 room1.friendly_print
-room1.find_reporters_with_skill("speaking")
+p room1.find_reporters_with_skill("grey hair")
+#p room1.reporters
 
 
 
