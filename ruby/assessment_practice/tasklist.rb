980d1e505ff4dab715b78ce7c2ec9a5c610928d9
Nikita Ahuja <nikitaahuja@Nikitas-MBP.home>
2017-09-01 02:29:50 -0500

    Need to debug release 3, start release 4, slightly debug 5, and complete release 6

--- ruby/assessment_practice/tasklist.rb
+++ ruby/assessment_practice/tasklist.rb
@@ -1,5 +1,5 @@
 class Task_list
-  attr_reader :owner
+  attr_reader :owner, :list
   attr_accessor :due_date
 
   def initialize (owner, due_date)
@@ -8,13 +8,42 @@ class Task_list
     @list = {} #location => task
   end
 
-  def new_location(location)
-    @list[location] = ""
+  def new_location(location, tasks)
+    if @list.has_key?(location)
+      puts "That location is already on the list!"
+    else
+      @list[location] = tasks
+    end
+  @list
+  end
+
+
+  def new_task(location, task)
+    if @list.has_value?(task)
+      puts "That task is already on the list!"
+    elsif @list.has_key?(location) && !(@list.has_value?(task))
+      @list[location] << task
+    else
+      @list[location] = task
+    end
+  @list
+  end
+
+  def print_pretty
+    @list.each do |location, task|
+      puts "At #{location}:"
+      puts "-#{task.join("\n")}"
+    end
   end
 
 end
 
 list = Task_list.new("Nikita", "August")
-list.new_location("Target")
-list.new_location("Walmart")
-p list
+#p list
+p list.new_location("Target", ["pens", "paper"])
+p list.new_location("Walmart", ["groceries", "makeup"])
+p list.new_location("Walmart", ["oranges", "pears"])
+p list.new_task("Walmart", ["groceries", "makeup"])
+p list.new_task("Office Max", ["supplies"])
+list.print_pretty
+
