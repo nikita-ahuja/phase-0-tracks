cfbe165908a56d6f8f81d6c88defc174dd260376
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-09-04 02:47:40 -0500

    Add variable to release 2

--- js/algos.js
+++ js/algos.js
@@ -121,9 +121,10 @@ console.log (object2_values2);
 
 // Write a function that takes an integer for length, and builds and returns an array of strings of the given length. So if we ran your function with an argument of 3, we would get an array of 3 random words back (the words don't have to be actual sensical English words -- "nnnnfph" totally counts). The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)
 
-// Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+// Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result
 
  function random_data(integer) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz"
   var string = ""
 
 
