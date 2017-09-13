adeea24e490ff3981aaff2b4c348d33c0513ff0d
Nikita Ahuja <nahuja038@gmail.com>
2017-09-12 21:42:03 -0500

    Add photo and play with script more

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,6 +1,31 @@
-function addGreenBorder() {
-  event.target.style.border = "2px solid green";
+function addBorder() {
+  event.target.style.border = "4px dashed black";
 }
 
-var photo = document.getElementById("icecream");
-photo.addEventListener("click", addGreenBorder);
+photos = document.getElementsByTagName("img");
+var photo0 = photos[0]
+var photo1 = photos[1]
+var photo2 = photos[2]
+
+//var photo = document.getElementById("icecream");
+photo0.addEventListener("click", addBorder);
+photo1.addEventListener("click", addBorder);
+photo2.addEventListener("click", addBorder);
+
+function biggerText() {
+  event.target.style.fontSize = "40px";
+}
+
+var headings = document.getElementsByTagName("h1");
+
+var heading0 = headings[0]
+heading0.addEventListener("click", biggerText);
+
+var heading1 = headings[1]
+heading1.addEventListener("click", biggerText);
+
+var heading2 = headings[2]
+heading2.addEventListener("click", biggerText);
+
+
+//document.getElementById("sadpizza").innerHTML="<img src = happypizza.jpg"/>";
