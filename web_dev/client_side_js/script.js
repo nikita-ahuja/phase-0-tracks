9e504c65810ca29d03e0b4bab28306dd50087d0e
Nikita Ahuja <nahuja038@gmail.com>
2017-09-12 23:06:19 -0500

    Change pizza from sad to happy

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,5 @@
+///////ADD BORDER FUNCTION AND APPLICATION//////////////
+
 function addBorder() {
   event.target.style.border = "4px dashed black";
 }
@@ -7,10 +9,11 @@ var photo0 = photos[0]
 var photo1 = photos[1]
 var photo2 = photos[2]
 
-//var photo = document.getElementById("icecream");
 photo0.addEventListener("click", addBorder);
 photo1.addEventListener("click", addBorder);
 photo2.addEventListener("click", addBorder);
+
+///////BIGGER TEXT FUNCTION AND APPLICATION//////////////
 
 function biggerText() {
   event.target.style.fontSize = "40px";
@@ -26,6 +29,21 @@ heading1.addEventListener("mouseover", biggerText);
 
 var heading2 = headings[2]
 heading2.addEventListener("mouseover", biggerText);
+
+function sadToHappy() {
+  document.getElementById("sadpizza").src="happypizza.jpg";
+}
+
+photo1.addEventListener("click", sadToHappy);
+
+// var new_photo = document.createElement("img");
+// img.src = "happypizza.jpg";
+// var src = document.getElementById("happypizza");
+
+// src.appendChild(new_photo);
+
+
+// ADD HAPPY PIZZA FUNCTION //
 
 
 //document.getElementById("sadpizza").innerHTML="<img src = happypizza.jpg"/>";
