be6bcefd44d828cd0eb24d12c0b74e8485d38884
Nikita Ahuja <nahuja038@gmail.com>
2017-09-13 15:10:33 -0500

    Add jQuery script to js file (not yet functioning)

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('button').on('click', function() {
+    var submit_msg = $('<p>You have clicked submit!</p>');
+    $('#test').append(submit_msg);
+    $('button').remove();
+    //alert("Hello World!");
+  });
+});
