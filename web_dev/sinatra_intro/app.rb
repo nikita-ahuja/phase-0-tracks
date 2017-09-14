02b689e1ee32dac4605c67a1f12c51fa9b46f4c3
Nikita Ahuja <nahuja038@gmail.com>
2017-09-14 01:18:37 -0500

    Add a contact route and great_job route draft

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,17 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact/:person' do
+  person = params[:person]
+  "#{person}'s contact information is person@gmail.com"
+end
+
+get '/great_job/:person' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
